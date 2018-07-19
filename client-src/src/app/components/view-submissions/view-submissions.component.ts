e4648c22925b6860ceb5ad5d129551a6295c2503
abhay <abhaytiwari713@gmail.com>
2018-07-19 22:39:26 +0530

    get submission front end done

--- client-src/src/app/components/view-submissions/view-submissions.component.ts
+++ client-src/src/app/components/view-submissions/view-submissions.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SubmissionService } from "../../services/submission.service";
 
 @Component({
-  selector: 'app-view-submissions',
-  templateUrl: './view-submissions.component.html',
-  styleUrls: ['./view-submissions.component.css']
+  selector: "app-view-submissions",
+  templateUrl: "./view-submissions.component.html",
+  styleUrls: ["./view-submissions.component.css"]
 })
 export class ViewSubmissionsComponent implements OnInit {
-
-  constructor() { }
+  message: string = "";
+  hideMessage: boolean = true;
+  candidates: any[] = [];
+  constructor(private submission: SubmissionService) {}
 
   ngOnInit() {
-  }
+    this.submission.getAllCandidates().subscribe(candidates => {
+      if (candidates.done == true) {
+        for (let i = 0; i < candidates.submissions.length; i++) {
+          this.candidates.push({
+            name: candidates.submissions[i].name,
+            experience: candidates.submissions[i].experience,
+            gender: candidates.submissions[i].gender,
+            location: candidates.submissions[i].jobLocation,
+            skills: candidates.submissions[i].skills.toString(),
+            expectedJoining: candidates.submissions[i].expectedJoining
+          });
+        }
 
+        console.log(this.candidates);
+      } else {
+        console.log("there is issue in loading ");
+      }
+    });
+  }
 }
---
+++ client-src/src/app/models/candidate.ts
@@ -0,0 +1,5 @@
+export interface candidate {
+  done: boolean;
+  submission?: Object;
+  error?: string;
+}
